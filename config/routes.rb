5bec2386518c10422b591d7c0a2600b38d4ac60e
Leonardo Perissinotto <l.perissinotto@donboscosandona.it>
2025-06-18 11:40:59 +0200

    fine index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get '/home', to: 'home#index', as: 'home'
+  get '/brasile', to: 'home#brasile', as: 'brasile'
+  get '/cile', to: 'home#cile', as: 'cile'
+  get '/peru', to: 'home#peru', as: 'peru'
+  get '/argentina', to: 'home#argentina', as: 'argentina'
+  get '/colombia', to: 'home#colombia', as: 'colombia'
+
   root 'home#index'
 end
